574ea7e9d53355c2ccde04a764eb02d8c901cee3
Julian Fajkowski <jfajkovv@gmail.com>
2022-01-17 22:49:21 +0100

    create BJ_Card class with value property

---
+++ blackjack-cli.py
@@ -0,0 +1,15 @@
+from text_games.py import *
+
+
+class BJ_Card(Card):
+    '''Regular blackjack game card.'''
+
+    @property
+    def value(self):
+        if self.face_up:
+            v = Deck.RANKS.index(self.rank) + 1
+            if v > 10:
+                v = 10
+        else:
+            v = None
+        return v
