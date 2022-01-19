76ef26212f9d0c8f267209df972c5ec37b55cc1c
Julian Fajkowski <jfajkovv@gmail.com>
2022-01-19 19:25:48 +0100

    set up BJ_Player and BJ_Dealer constructors

--- blackjack-cli.py
+++ blackjack-cli.py
@@ -9,12 +9,19 @@ class BJ_Hand(text_games.Hand):
     '''Collection of cards to do something with.'''
 
 
-class BJ_Player(text_games.Player):
+class BJ_Player(text_games.Player, BJ_Hand):
     '''Blackjack game player representation.'''
 
+    def __init__(self, name):
+        super().__init__(name)
+        self.card_set = []
 
-class BJ_Dealer(text_games.Player):
+class BJ_Dealer(text_games.Player, BJ_Hand):
     '''Casino house host representation.'''
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.card_set = []
 
 
 class BJ_Deck(text_games.Deck):
@@ -26,7 +33,13 @@ class BJ_Game(object):
 
     def __init__(self):
         the_player = BJ_Player(name='Player')
+        print(the_player)
+        print(the_player.name)
+        print(dir(the_player))
         the_dealer = BJ_Dealer(name='Croupier')
+        print(the_dealer)
+        print(the_dealer.name)
+        print(dir(the_dealer))
         cards_deck = BJ_Deck()
         cards_deck.fill_in(stacks=2)
         cards_deck.shuffle()
